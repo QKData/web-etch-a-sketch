4eb449c953f913229c4068efd19421de2d8b6799
QKData <quangkien.md@gmail.com>
2024-12-01 15:35:16 +0700

    Initial commit

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
-// Create 256 cells
-for (let i = 1; i <= 16 * 16; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add("cell");
+button.addEventListener("click", (e) => {
+    // Clear existing grid
+    container.innerHTML = "";
+    
+    // Create n * n cells
+    let n = prompt("How many squares per side do you want? (Max 100)");
+    
+    // Validate input
+    n = parseInt(n);
+    if (n > 0 && n <= 100) {  
+        for (let i = 1; i <= n * n; i++) {
+            const cell = document.createElement('div');
+            cell.classList.add("cell");
 
-    // Set up hovering effect
-    cell.addEventListener("mouseover", (e) => {
-        e.target.classList.add("colored");
-    });
+            // Calculate new cell size based on container size
+            cell.style.width = `calc(100% / ${n})`;
+            cell.style.height = `calc(100% / ${n})`;
 
-    // Add cells to container
-    container.appendChild(cell);
-};
+            // Set up hovering effect (random color)
+            cell.addEventListener("mouseover", (e) => {
+                if (!e.target.style.backgroundColor) {
+                    e.target.style.backgroundColor = getRandomColor();
+                };
+            });
+
+            // Add cells to container
+            container.appendChild(cell);
+        };
+    } else {
+        alert("Please enter a number between 1 and 100");
+    };
+});
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
